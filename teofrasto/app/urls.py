9d3b7f19d3feba85c5f4ccf34e7191ea14aa97b3
Ivan Chavez <chavezromeroivan@gmail.com>
2022-08-13 23:35:08 -0600

    Create viewsets and routers

--- teofrasto/app/urls.py
+++ teofrasto/app/urls.py
@@ -1,14 +1,11 @@
 from django.urls import path
-
+from rest_framework.routers import DefaultRouter
 from . import views
+router  = DefaultRouter()
+router.register('architects', views.ArchitectViewSet)
+router.register('firms', views.FirmViewSet)
+router.register('projects', views.ProjectViewSet)
+router.register('clients', views.ClientViewSet)
 
-urlpatterns = [
-    path('architects',views.ArchitectList.as_view()),
-    path('architects/<int:pk>/', views.Architects.as_view()),
-    path('firms', views.FirmList.as_view()),
-    path('firms/<int:pk>/', views.Firms.as_view()),
-    path('projects', views.ProjectList.as_view()),
-    path('projects/<int:pk>/', views.Projects.as_view()),
-    path('clients', views.ClientList.as_view()),
-    path('clients/<int:pk>/', views.Clients.as_view())
-    ]
+
+urlpatterns = router.urls
--- teofrasto/app/views.py
+++ teofrasto/app/views.py
@@ -1,56 +1,37 @@
-from itertools import product
-from django.shortcuts import get_object_or_404
-from django.http import HttpResponse
-from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework.views import APIView
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.viewsets import ModelViewSet
 from .models import Architect, Firm, Project, Client
 from .serializers import ArchitectSerializer, FirmSerializer, ProjectSerializer, ClientSerializer
 from rest_framework import status
 
 # Create your views here.
-class Architects(RetrieveUpdateDestroyAPIView):
-    queryset = Architect.objects.all()
-    serializer_class = ArchitectSerializer
 
-
-class Firms(RetrieveUpdateDestroyAPIView):
+class FirmViewSet(ModelViewSet):
     queryset = Firm.objects.all()
     serializer_class = FirmSerializer
-    
-    def delete(self, request, pk):
-        firm  = get_object_or_404(Firm, pk=pk)
-        if firm.client_set.count() > 0 or firm.architect_set.count() > 0:
+
+    def destroy(self, request, *args, **kwargs):
+        if Client.objects.filter(firm_id=kwargs['pk']).count() > 0 or Architect.objects.filter(firm_id=kwargs['pk']).count() > 0:
             return Response({'error': 'firm cannot be eliminated because it is associated with another model'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
-        firm.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        return super().destroy(request, *args, **kwargs)
+    
 
 
-class Projects(RetrieveUpdateDestroyAPIView):
+class ProjectViewSet(ModelViewSet):
    queryset = Project.objects.all()
    serializer_class = ProjectSerializer
 
 
-
-class Clients(RetrieveUpdateDestroyAPIView):
+class ClientViewSet(ModelViewSet):
     queryset = Client.objects.all()
     serializer_class = ClientSerializer
   
 
-class FirmList(ListCreateAPIView):
-    queryset = Firm.objects.all()
-    serializer_class = FirmSerializer
-   
-class ArchitectList(ListCreateAPIView):
+class ArchitectViewSet(ModelViewSet):
     queryset = Architect.objects.select_related('firm').prefetch_related('project').all()
     serializer_class = ArchitectSerializer
 
-class ProjectList(ListCreateAPIView):
-    queryset = Project.objects.all()
-    serializer_class  = ProjectSerializer
+
         
-class ClientList(ListCreateAPIView):
-    queryset = Client.objects.all()
-    serializer_class  = ClientSerializer
+
         
